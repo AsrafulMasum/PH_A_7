26736ed00d532b0a11ff88c1d5f99cad30257fc9
AsrafulMasum <h.m.asrafulmasum@gmail.com>
2023-09-15 08:55:47 +0600

    onclicking select button course name added to the cart ui

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -4,10 +4,18 @@ const Cart = ({cart}) => {
   console.log(cart);
   return (
     <div>
-      <div className="card bg-base-100">
-          <div className="card-body items-center text-center">
-            <h2 className="card-title">Shoes!</h2>
-            <p>If a dog chews shoes whose shoes does he choose?</p>
+      <div className="card bg-base-100 p-4">
+          <div className="p-4">
+            <hr />
+            <div className="py-4">
+              <h2 className="text-[#1C1B1B] text-xl font-bold pb-4">Course Name</h2>
+              <ol>
+                {
+                  cart.map((item, index) => <li className="list-decimal list-inside text-[#1C1B1B99]" key={index}>{item.course_name}</li>)
+                }
+              </ol>
+            </div>
+            <hr />
           </div>
         </div>
     </div>
