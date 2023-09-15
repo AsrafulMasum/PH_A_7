03a1ae7b8276ef0f7faeb1b543b4ca5abb254893
AsrafulMasum <h.m.asrafulmasum@gmail.com>
2023-09-15 21:44:43 +0600

    last checkup before submit

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,7 +3,7 @@ const Nav = () => {
   return (
     <div>
       <div className="navbar justify-center">
-        <a onClick={() => window.location.reload()} className="btn btn-ghost normal-case text-2xl font-bold text-[#1C1B1B]">Course Registration</a>
+        <a onClick={() => window.location.reload()} className="cursor-pointer normal-case text-2xl font-bold text-[#1C1B1B]">Course Registration</a>
       </div>
     </div>
   );
