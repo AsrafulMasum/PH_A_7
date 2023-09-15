dd0c465891f4e2575a800e64bdc603737535f54b
AsrafulMasum <h.m.asrafulmasum@gmail.com>
2023-09-15 06:03:46 +0600

    fonts,icons,favicon and navbar attacted

---
+++ src/components/Nav.jsx
@@ -0,0 +1,12 @@
+
+const Nav = () => {
+  return (
+    <div>
+      <div className="navbar justify-center bg-base-100">
+        <a className="btn btn-ghost normal-case text-2xl font-bold text-[#1C1B1B]">Course Registration</a>
+      </div>
+    </div>
+  );
+};
+
+export default Nav;
